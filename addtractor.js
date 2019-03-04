3bdcd1e56dbf86f97b04aa691500fac560c2af7b
Topik <topik@rayyic.xyz>
2019-03-03 18:15:54 -0600

    erg

---
+++ addtractor.js
@@ -0,0 +1,13 @@
+$(window).load(function(){
+$(document).ready(function() {
+  //  addTractor();
+  //  $("#firstNumber, #secondNumber").on("keydown keyup", function() {
+  //      addTractor();
+  //  });
+});
+
+function addTractor() {
+      var firstPoly = "2x + 3";
+      var secondPoly = "8x - 4";
+      var addedPolys = firstPoly + secondPoly;
+});
